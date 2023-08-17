94776c7cea63740f6b1eeee25a3fb8f1e55a5ea2
jando99 <fernandovalle99@gmail.com>
2023-08-16 18:01:33 -0700

    final commit with added antd

--- app/page.js
+++ app/page.js
@@ -10,14 +10,13 @@
 
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
 import Map from 'react-map-gl';
-import Head from 'next/head'
-import Image from 'next/image'
-
-import DeckGL from '@deck.gl/react/typed';
-import {ScatterplotLayer} from '@deck.gl/layers/typed';
-import {HexagonLayer} from '@deck.gl/aggregation-layers';
+import DeckGL from '@deck.gl/react';
+import { ScatterplotLayer } from '@deck.gl/layers';
+import { HexagonLayer } from '@deck.gl/aggregation-layers';
+import { Select, Spin } from 'antd';
 import { TripsLayer } from '@deck.gl/geo-layers';
 
 //map token
@@ -37,12 +36,12 @@ const getDataset = (dataset) => {
         getPosition: d => [parseFloat(d.longitude), parseFloat(d.latitude)],
       };
     default:
-      throw new Error('Invalid dataset!');
+      throw new Error('Invalid dataset');
   }
 };
 
 //Scatterplot function
-const scatterplotLayer = (city) => {
+const scatterplotLayer = (city, finishLoading) => {
   const dataset = getDataset(city);
 
   return new ScatterplotLayer({
@@ -58,13 +57,14 @@ const scatterplotLayer = (city) => {
     radiusMinPixels: 1,
     radiusMaxPixels: 100,
     lineWidthMinPixels: 1,
-    getFillColor: d => [255, 140, 0],
-    getLineColor: d => [255, 0, 0],
+    getFillColor: () => [255, 140, 0],
+    getLineColor: () => [255, 0, 0],
+    onDataLoad: finishLoading,
   });
 };
 
-//Hexagon Layer
-const hexagonLayer = (city) => {
+//Hexagon fucntion
+const hexagonLayer = (city, finishLoading) => {
   const dataset = getDataset(city);
   return new HexagonLayer({
     id: 'hexagon-layer',
@@ -74,7 +74,7 @@ const hexagonLayer = (city) => {
     extruded: true,
     radius: 75,
     elevationScale: 10,
-    opacity: 0.2, 
+    opacity: 0.2,
     colorRange: [
       [255,255,178],
       [254,217,118],
@@ -83,35 +83,56 @@ const hexagonLayer = (city) => {
       [240,59,32],
       [189, 0, 38],
     ],
+    onDataLoad: () => {
+      finishLoading();
+    },
   });
+};
+
+//function viewstate to show NYC area once user clicks on option
+const NYC_VIEW_STATE = {
+  longitude: -74.0021069,
+  latitude: 40.7423867,
+  zoom: 12,
+};
+
+//function viewstate to show LA area once user clicks on option
+const LA_VIEW_STATE = {
+  longitude: -118.44886,
+  latitude: 34.182652,
+  zoom: 10,
 };
 
 //Main Function
 export default function Home() {
   const [selectedDataset, setSelectedDataset] = useState('nyc');
   const [selectedLayer, setSelectedLayer] = useState('scatterplot');
+  const [loading, setLoading] = useState(false);
 
   const [layers, setLayers] = useState([]);
   const [ref, setRef] = useState(null);
+
+  const finishLoading = () => {
+    setLoading(false);
+  };
 
   //useeffect function to choose to view any of the 3 layers.
   useEffect(() => {
     switch (selectedLayer) {
       case 'scatterplot':
         if (ref) clearInterval(ref);
-        setLayers([scatterplotLayer(selectedDataset)]);
+        setLayers([scatterplotLayer(selectedDataset, finishLoading)]);
         break;
       case 'hexagon':
         if (ref) clearInterval(ref);
-        setLayers([hexagonLayer(selectedDataset)]);
+        setLayers([hexagonLayer(selectedDataset, finishLoading)]);
         break;
       case 'trips':
-        const timer = setInterval(() => { //Trips function to display uber trips data
+        const timer = setInterval(() => {
           setLayers([
             new TripsLayer({
               id: 'trips',
-              data:
-                'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips-v7.json',
+              data: 'https://raw.githubusercontent.com/uber-common/deck.gl-data/master/examples/trips/trips-v7.json',
               getPath: (d) => {
                 return d.path;
               },
@@ -127,6 +148,7 @@ export default function Home() {
               trailLength: 180,
               currentTime: (performance.now() % 20000) / 10,
               shadowEnabled: false,
+              onDataLoad: finishLoading,
             }),
           ]);
         }, 50);
@@ -138,84 +160,105 @@ export default function Home() {
     }
   }, [selectedLayer, selectedDataset]);
 
-  const handleLayerChange = (x) => {
-    const newSelectedLayer = x.target.value;
-    setSelectedLayer(newSelectedLayer);
+  const handleLayerChange = (value) => {
+    setSelectedLayer(value);
   };
 
-  const handleDatasetChange = (x) => {
-    const newSelectedDataset = x.target.value;
-    setSelectedDataset(newSelectedDataset);
+  const handleDatasetChange = (value) => {
+    setLoading(true);
+    setSelectedDataset(value);
   };
 
-  // line 151 to 171 is pop up window function to help users if they are confused with app functions
   const [showPopup, setShowPopup] = useState(false);
 
+  //line 177 to 203 is a support popup menu if user is confused with program
   const handlePopupToggle = () => {
-    console.log("Toggling pop-up visibility");
     setShowPopup(!showPopup);
   };
 
   const Popup = () => {
     return (
       <div
-        style={{position: 'absolute', top: '60px', left: '20px', background: 'rgba(208, 211, 212, 0.5)', border: '5px solid #00ffff', padding: '8px', borderRadius: '4px', zIndex: 1, }}
+        style={{
+          position: 'absolute',
+          top: '60px',
+          left: '20px',
+          background: 'rgba(208, 211, 212, 0.5)',
+          border: '5px solid #00ffff',
+          padding: '8px',
+          borderRadius: '4px',
+          zIndex: 1,
+        }}
       >
-        <p style={{color:'rgb(249, 253, 0)'}}>
-          Welcome to CitySync Support! <br/>
-          Please choose a layer and dataset from the dropdowns,<br/>
-          then move to either Los Angeles Area or New York City to view layers and data!<br/>
-          Click <span style={{color:'#00ffff'}}>Help?</span> to Close!
+        <p style={{ color: 'rgb(249, 253, 0)' }}>
+          Welcome to CitySync Support! <br />
+          Please choose a layer and dataset from the dropdowns,<br />
+          then it will direct you to Los Angeles Area or New York City to view layers and data!<br />
+          Click <span style={{ color: '#00ffff' }}>Help?</span> to Close!
         </p>
       </div>
     );
   };
-  
-  return ( //html5 with embedded css to show layers and data
+
+  //chooses either viewstate
+  const viewState = selectedLayer === 'trips' || selectedDataset === 'nyc' ? NYC_VIEW_STATE : LA_VIEW_STATE;
+
+  return ( //html5 with embedded css to show layers and data. Added Select and Spin from antd
     <main>
-      <Head>  
+      <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <DeckGL
-          layers={[layers]}
-          controller
-          initialViewState={{
-            longitude: -74.0021069,
-            latitude: 40.7423867,
-            zoom: 12
-          }}
-          id="deckgl-overlay"
-        >
-          {showPopup && <Popup />}
-          <div style={{ position: 'absolute', height: 60, width: '100%', backgroundColor: 'rgba(174, 182, 191, 0.2)', justifyContent: 'center',display: 'flex', flexDirection: 'row', alignItems: 'center', padding: 16, gap: 8 }}>
-            <h2 style={{color:'rgb(249, 253, 0)'}}>Choose your Layer and Data!</h2> <br/>
-            <span style={{color:'rgb(249, 253, 0)'}}>Layer: </span>
-            <select onChange={handleLayerChange} value={selectedLayer} style={{ backgroundColor: 'rgba(174, 182, 191, 0.2)', color: '#00ffff', padding: '8px', border: 'none' }}>
-              <option value="scatterplot" className="custom-option">Scatterplot Layer</option>
-              <option value="hexagon" className="custom-option">Hexagon Layer</option>
-              <option value="trips" className="custom-option">Traffic Layer</option>
-            </select>
-            <span style={{color:'rgb(249, 253, 0)'}}>Dataset: </span>
-              {selectedLayer === 'trips' ? (
-                <div style={{color:'#00ffff'}}>NYC</div>
-              ) : (
-            <select onChange={handleDatasetChange} value={selectedDataset} style={{ backgroundColor: 'rgba(174, 182, 191, 0.2)', color: '#00ffff', padding: '8px', border: 'none' }}>
-              <option value="la" className="custom-option">LA Active Businesses</option>
-              <option value="nyc" className="custom-option">NYC Trees</option>
-            </select>
-            )}
-            <br/>
-            <button style={{color:'#00ffff'}} onClick={handlePopupToggle}>Help?</button>
-          </div>
-          <Map
-            mapboxAccessToken={mapBoxToken}
-            style={{ width: '100vw', height: '100vh' }}
-            mapStyle="mapbox://styles/mapbox/dark-v9"
+        layers={layers}
+        controller
+        initialViewState={viewState}
+        id="deckgl-overlay"
+      >
+        {showPopup && <Popup />}
+        <div style={{ position: 'absolute', height: 60, width: '100%', backgroundColor: 'rgba(174, 182, 191, 0.2)', justifyContent: 'center', display: 'flex', flexDirection: 'row', alignItems: 'center', padding: 16, gap: 8 }}>
+          <h2 style={{ color: 'rgb(249, 253, 0)', paddingRight: '8em' }}>CitySync: Choose your Layer and Data!</h2>
+          <span style={{ color: 'rgb(249, 253, 0)' }}>Layer: </span>
+          <Select
+            value={selectedLayer}
+            style={{ 
+              padding: '8px', 
+              border: 'none' }}
+            onChange={handleLayerChange}
+            options={[
+              { value: 'scatterplot', label: 'Scatterplot', className : "custom-option" },
+              { value: 'hexagon', label: 'Hexagon', className : "custom-option" },
+              { value: 'trips', label: 'Trips', className : "custom-option" },
+            ]}
           />
-        </DeckGL>
-        <footer style={{position: 'absolute', bottom: 0, left: 0, width: '100%', textAlign: 'center', backgroundColor: 'rgba(174, 182, 191, 0.2)', color: 'rgb(249, 253, 0)', padding: '8px 0',}}
-          >© {new Date().getFullYear()} Copyright by <a href="https://github.com/jando99/mapgl1.git"> Fernando Valle </a>
-        </footer>
+          <span style={{ color: 'rgb(249, 253, 0)' }}>Dataset: </span>
+          {selectedLayer === 'trips' ? <div style={{ color: '#00ffff' }}>NYC</div> : (
+            <Select
+              value={selectedDataset}
+              style={{  
+                padding: '8px', 
+                border: 'none' }}
+              onChange={handleDatasetChange}
+              options={[
+                { value: 'la', label: 'LA Active Businesses', className:"custom-option" },
+                { value: 'nyc', label: 'NYC Trees', className:"custom-option" },
+              ]}
+            />
+          )}
+          <br />
+        <button style={{ color: '#00ffff' }} onClick={handlePopupToggle}>Help?</button>
+        </div>
+        <Map
+          mapboxAccessToken={mapBoxToken}
+          style={{ width: '100vw', height: '100vh' }}
+          mapStyle="mapbox://styles/mapbox/dark-v9"
+        />
+      </DeckGL>
+      {loading && <div style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0, backgroundColor: 'white', opacity: 0.6, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <Spin size="large" />
+      </div>}
+      <footer style={{ position: 'absolute', bottom: 0, left: 0, width: '100%', textAlign: 'center', backgroundColor: 'rgba(174, 182, 191, 0.2)', color: 'rgb(249, 253, 0)', padding: '8px 0', }}>
+        © {new Date().getFullYear()} Copyright by <a href="https://github.com/jando99/mapgl1.git"> Fernando Valle </a>
+      </footer>
     </main>
-  )
+  );
 }
